17355b6336dc4c6969e0ae3e73a8e398d2fbff5b
Poltiev <poltiev@yandex.ru>
2018-09-15 00:31:48 +0300

    in registration

--- js/front.js
+++ js/front.js
@@ -5,3 +5,9 @@ $('.burger').on('click', function(){
 	$(this).next().toggleClass('active');
 	$(this).toggleClass('active');
 });
+// tabs
+$('ul.tabs-caption').on('click', 'li:not(.active)', function() {
+	$(this)
+		.addClass('active').siblings().removeClass('active')
+		.closest('div.tabs').find('div.tabs-content').removeClass('active').eq($(this).index()).addClass('active');
+});
